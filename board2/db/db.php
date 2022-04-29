389eecb287ec80250b0147071fc9332f0bfb30cc
username <thswlgus15@hanmail.net>
2022-04-29 11:20:56 +0900

    중간수정

--- board2/db/db.php
+++ board2/db/db.php
@@ -2,7 +2,7 @@
 define("URL", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "506greendg@");
-define("DB_NAME", "board_login");
+define("DB_NAME", "board2");
 
 function get_conn() {
     return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
