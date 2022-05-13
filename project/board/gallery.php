9703a238e641cad7c7fb8398400fa9c80a96cfc0
username <thswlgus15@hanmail.net>
2022-05-13 12:07:45 +0900

    modify 'gallery'

--- project/board/gallery.php
+++ project/board/gallery.php
@@ -17,17 +17,17 @@
                 <aside class="left">
                     <h4>LOCATION</h4>
                     <ul>
-                        <li><a target="iframe1" href="index.php">서울</a></li>
-                        <li><a target="iframe1" href="../user/join.php">대구</a></li>
-                        <li><a target="iframe1" href="../user/login.php">부산</a></li>
-                        <li><a target="iframe1" href="../admin/upload.php">제주</a></li>
+                        <li><a target="iframe1" href="s_gallery.php">서울</a></li>
+                        <li><a target="iframe1" href="d_gallery.php">대구</a></li>
+                        <li><a target="iframe1" href="b_gallery.php">부산</a></li>
+                        <li><a target="iframe1" href="j_gallery.php">제주</a></li>
                     </ul>
                 </aside>
             </div>
             <div class="contents">
                 <section id="main">
                     <article>
-                        <iframe name="iframe1" src="index.php" frameborder="0" width="1100" height="700"></iframe>
+                        <iframe name="iframe1" src="s_gallery.php" frameborder="0" height="700"></iframe>
                     </article>
                 </section>
             </div>
---
+++ project/board/s_gallery.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/local_gallery.css">
+    <title>서울 미술관</title>
+</head>
+<body>
+    <table class="gallery">
+        <tr>
+            <td class="gallery_pic"><img src="../img/gallery_pic.PNG"></td>
+            <td class="gallery_pic"><img src="../img/gallery_pic.PNG"></td>
+            <td class="gallery_pic"><img src="../img/gallery_pic.PNG"></td>
+        </tr>
+        <tr>
+            <td class="gallery_nm">서울 미술관 1</td>
+            <td class="gallery_nm">서울 미술관 2</td>
+            <td class="gallery_nm">서울 미술관 3</td>
+        </tr>
+        <tr>
+            <td class="gallery_intro"><img src="../img/gallery_intro.PNG"></td>
+            <td class="gallery_intro"><img src="../img/gallery_intro.PNG"></td>
+            <td class="gallery_intro"><img src="../img/gallery_intro.PNG"></td>
+        </tr>
+    </table>
+</body>
+</html>
